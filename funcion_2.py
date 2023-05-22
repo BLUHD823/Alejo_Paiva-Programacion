60c9c21a51acf37146a34be9ad81536ed19b756e
unknown <alejodeje@gmail.com>
2023-05-22 20:00:10 -0300

    cambios

--- funcion_2.py
+++ funcion_2.py
@@ -1,8 +1,9 @@
 import re
 
 def reemplazar_caracteres(cadena,letra_1,letra_2):
+    contador = cadena.count("o")
     reemplazo = re.sub(letra_1,letra_2,cadena,2)
-    return reemplazo
+    return reemplazo,contador
 
 print(reemplazar_caracteres("Hola Mundo","o","a"))
 
