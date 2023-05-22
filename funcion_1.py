ebb572bfa9c722feeff9825c1ae5e59c697ee605
unknown <alejodeje@gmail.com>
2023-05-22 19:57:03 -0300

    Cambios

--- funcion_1.py
+++ funcion_1.py
@@ -1,10 +1,8 @@
-
-
 def aplicar_aumento(dato):
     porcentaje = dato + (dato*0.05)
     print(porcentaje)
 
-# dato = float(input("Ingrese el número: "))
-# aplicar_aumento(dato)
+dato = float(input("Ingrese el número: "))
+aplicar_aumento(dato)
 
 
